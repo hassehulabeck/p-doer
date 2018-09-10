699876c5979b8f6bed4a81e8f0b687d10bb71543
hassehulabeck <hansbudandersson@gmail.com>
2018-09-10 09:53:22 +0200

    Added db-connection

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
+try {
+  $dbh = new PDO("mysql: host=localhost; dbname=datorbutiken", "cpu", "brOmmablOcks");
+}
+catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
 
+// Förbered en query.
+$stmt = $dbh->prepare("
+  SELECT *
+  FROM laptop
+  WHERE 1
+");
 
+// Kör queryn
+$stmt->execute();
 
-
+// Plocka fram datan.
+print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
 ?>
